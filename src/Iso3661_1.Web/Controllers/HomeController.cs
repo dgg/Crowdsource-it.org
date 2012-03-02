52d13f0255b34b3e71fa9e47e1fcb3d7153c3056
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-03-02 15:48:40 +0100

    empty Mvc3 application

---
+++ src/Iso3661_1.Web/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Iso3661_1.Crowdsource_it.org.Web.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			return View();
+		}
+	}
+}
