84baeffa55448eaf2fc124e65fed3178c85a39d6
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-03-02 21:54:31 +0100

    showcased request attributes to perform rudimentary validation

---
+++ src/Iso3166_1.Web/Api/Infrastructure/PrepareCountryQueryAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using Iso3166_1.Crowdsource_it.org.Web.Api.Messages;
+using ServiceStack.ServiceHost;
+
+namespace Iso3166_1.Crowdsource_it.org.Web.Api.Infrastructure
+{
+	public class PrepareCountryQueryAttribute : Attribute, IHasRequestFilter
+	{
+		public void RequestFilter(IHttpRequest req, IHttpResponse res, object requestDto)
+		{
+			var msg = requestDto as Country;
+			if (msg == null ||
+				string.IsNullOrEmpty(msg.Code) ||
+				(!msg.IsNumeric() && (msg.Code.Length < 2 || msg.Code.Length > 3)))
+			{
+				res.StatusCode = (int)HttpStatusCode.BadRequest;
+				res.End();
+			}
+		}
+	}
+}
--- src/Iso3166_1.Web/Api/Messages/Country.cs
+++ src/Iso3166_1.Web/Api/Messages/Country.cs
@@ -1,11 +1,21 @@
-﻿using ServiceStack.ServiceHost;
+﻿using System.Globalization;
+using Iso3166_1.Crowdsource_it.org.Web.Api.Infrastructure;
+using ServiceStack.ServiceHost;
 
 namespace Iso3166_1.Crowdsource_it.org.Web.Api.Messages
 {
 	// the route is defined in the message
 	[RestService("/country/{code}", "GET, HEAD")]
+	[PrepareCountryQuery]
 	public class Country
 	{
 		public string Code { get; set; }
+
+		public bool IsNumeric()
+		{
+			short numericCode;
+			bool parsed = short.TryParse(Code, NumberStyles.Integer, CultureInfo.InvariantCulture, out numericCode);
+			return parsed && numericCode > 0;
+		}
 	}
 }
