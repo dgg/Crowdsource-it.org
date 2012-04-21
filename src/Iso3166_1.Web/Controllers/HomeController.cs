5d28cd920181f65dad939c692c3c08225113422e
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2012-04-21 17:58:38 +0200

    test for presentation inside the web application itself

--- src/Iso3166_1.Web/Controllers/HomeController.cs
+++ src/Iso3166_1.Web/Controllers/HomeController.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
-using ServiceStack.ServiceClient.Web;
+﻿using System.Web.Mvc;
 
 namespace Iso3166_1.Crowdsource_it.org.Web.Controllers
 {
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Anug()
 		{
 			return View();
 		}
---
+++ src/Iso3166_1.Web/Scripts/pages/Anug.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="/Scripts/jquery-1.7.2.js" />
+/// <reference path="/Scripts/jquery.validate.js" />
+
+$(function () {
+	$('#slides').presentation({pagerClass: 'hide', prevNextClass: 'hide'});
+});
